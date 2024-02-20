b332e336589ec6718165cac1d35f976a9a86f037
WowCodeSusah <114371708+WowCodeSusah@users.noreply.github.com>
2024-02-20 11:23:14 +0700

    Add files via upload

---
+++ yess.js
@@ -0,0 +1,35 @@
+document.getElementById('michael').addEventListener('click', function() {
+    window.location.href = 'michael.html';
+  });
+
+document.getElementById('rafael').addEventListener('click', function() {
+    window.location.href = 'rafa.html';
+  });
+
+document.getElementById('raihan').addEventListener('click', function() {
+    window.location.href = 'raihan.html';
+  });
+
+document.getElementById('louis').addEventListener('click', function() {
+    window.location.href = 'louis.html';
+  });
+
+document.getElementById('darryl').addEventListener('click', function() {
+    window.location.href = 'daryl.html';
+  });
+
+document.getElementById('christoffer').addEventListener('click', function() {
+    window.location.href = 'chris.html';
+  });
+
+document.getElementById('ostein').addEventListener('click', function() {
+    window.location.href = 'ostein.html';
+  });
+
+document.getElementById('prigorian').addEventListener('click', function() {
+    window.location.href = 'file name here';
+  });
+
+document.getElementById('shaan').addEventListener('click', function() {
+    window.location.href = 'file name here';
+  });
